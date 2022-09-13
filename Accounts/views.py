a5d34b93ccda0d811d152dce2306468b8bf16eb3
Prateek-VIT <82014373+Prateek-VIT@users.noreply.github.com>
2022-09-13 18:22:02 +0530

    Add files via upload

--- Accounts/views.py
+++ Accounts/views.py
@@ -4,13 +4,35 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-        return render(request,'Accounts/index.html') #index
+    return render(request,'Accounts/index.html') #index
 
 def adminlogin(request):
-        return render(request,'Accounts/adminlogin.html')
+    return render(request,'Accounts/adminlogin.html')
 
 def adminhp(request):
-        return render(request,'Accounts/adminhp.html')
+    return render(request,'Accounts/adminhp.html')
 
 def faculty_page(request):
     return render(request,"Accounts/faculty_page.html")
+
+def facultyinfo(request):
+    return render(request,'Accounts/facultyinfo.html')
+        
+def facultyinfoupload(request):
+    return render(request,'Accounts/facultyinfoupload.html')
+
+def uploadcoursesadmin(request):
+    return render(request,'Accounts/uploadcourseadmin.html')
+
+def viewaddcourses(request):
+    return render(request,'Accounts/viewaddcourses.html')
+
+def viewfacpref(request):
+    return render(request,'Accounts/viewfacpref.html')
+
+def wishlistuploadadmin(request):
+    return render(request,'Accounts/wishlistuploadadmin.html')
+
+def wishlistviewadmin(request):
+    return render(request,'Accounts/wishlistviewadmin.html')
+
