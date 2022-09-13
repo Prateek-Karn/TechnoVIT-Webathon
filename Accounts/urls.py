e535d93dae5f9ecbb63d7d5671221702338fd476
Prateek-VIT <82014373+Prateek-VIT@users.noreply.github.com>
2022-09-13 20:11:30 +0530

    Add files via upload

--- Accounts/urls.py
+++ Accounts/urls.py
@@ -2,7 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
-    path('adminLogin/', views.adminlogin),
-    path('adminhp/', views.adminhp),
+    path('', views.home, name='home'),
+    path('adminLogin/', views.adminlogin,name='adminLogin'),
+    path('adminhp/', views.adminhp, name='adminhp'),
+    path('faculty_page/', views.faculty_page,name='faculty_page'),
+    path('facultyinfo', views.facultyinfo,name='facultyinfo'),
+    path('facultyinfoupload/', views.facultyinfoupload, name='facultyinfoupload'),
+    path('uploadcoursesadmin/', views.uploadcoursesadmin,name='uploadcoursesadmin'),
+    path('viewaddcourses/', views.viewaddcourses, name='viewaddcourses'),
+    path('viewfacpref/', views.viewfacpref, name='viewfacpref'),
+    path('wishlistuploadadmin/', views.wishlistuploadadmin, name='wishlistuploadadmin'),
+    path('wishlistviewadmin/', views.wishlistviewadmin, name='wishlistviewadmin'),
+
 ]
